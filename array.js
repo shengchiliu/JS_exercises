8b7397f6450f3faed2bee42e0c52604050afafcb
shengchiliu <shengchiliu@cam.ac.uk>
2018-05-22 16:12:33 +0100

    modified

--- array.js
+++ array.js
@@ -5,11 +5,16 @@
 let arr
 (arr = []).length = 3;
 arr.fill(0);
-
 console.log(arr)
-
 
 // Method 2
 new Array(3).fill(0);
-
 console.log(arr)
+
+
+// Clone Array
+R = Q.slice(0);
+
+// Last Element of Array
+R[R.length-1] = 1;
+
