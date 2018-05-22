3180a951f60c29d87bc47c383f89be1372188db5
shengchiliu <scl57@cam.ac.uk>
2018-05-22 23:09:13 +0100

    modified

--- array.js
+++ array.js
@@ -15,6 +15,21 @@ console.log(arr)
 // Clone Array
 R = Q.slice(0);
 
+
 // Last Element of Array
 R[R.length-1] = 1;	//R.pop();
 
+
+// Find Index of Maximum
+arr = [1,2,3,4,5];
+arr.indexOf(Math.max(...arr))
+
+
+// All Zeros
+let Q = [0,0,0];
+
+function allZeros(element) {
+    return element == 0;
+}
+
+console.log(Q.every(allZeros))
